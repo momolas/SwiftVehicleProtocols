e80e84aa33fd4323f526234c470e1d451c9e4bf9
Momo L'As <momolas@users.noreply.github.com>
2026-08-30 06:09:12 +0200

    Add AnyObject conformance and default sendRawCAN implementation to VehicleInterface

--- Sources/VehicleTransport/VehicleInterface.swift
+++ Sources/VehicleTransport/VehicleInterface.swift
@@ -1,8 +1,14 @@
 import Foundation
 import VehicleCore
 
-public protocol VehicleInterface: Sendable {
+public protocol VehicleInterface: AnyObject, Sendable {
     func sendRawCAN(id: UInt32, data: Data, bus: UInt8) async throws
     func sendDiagnosticRequest(_ requestHex: String, timeout: TimeInterval) async throws -> String
     func setTarget(txID: String, rxID: String?) async throws
+}
+
+public extension VehicleInterface {
+    func sendRawCAN(id: UInt32, data: Data, bus: UInt8) async throws {
+        // Optionnel pour les interfaces ne supportant que la couche diagnostique (ex: ELM327)
+    }
 }
